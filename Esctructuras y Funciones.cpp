fc09ec81052bc4d23ffef87a2e598ec829218f66
a25310145-hue <a25310145@ceti.mx>
2026-03-05 17:17:40 -0600

    hice cambios a este
    agregue variable para ver si habia datos

--- Esctructuras y Funciones.cpp
+++ Esctructuras y Funciones.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// ----- ESTRUCTURA ANIDADA -----
 struct Tarjeta {
     char serie[50];
     int numPuertos;
@@ -12,60 +13,80 @@ struct Tarjeta {
     float largo;
 };
 
+// ----- ESTRUCTURA PRINCIPAL -----
 struct PC {
     char marca[50];
     char modelo[50];
     int ram;
     float velocidadProcesador;
+
+    // estructura anidada dentro de PC
     Tarjeta tarjeta;
 };
 
-// eliminar espacios al inicio
-void quitarEspaciosInicio(char *cadena){
-    int i=0;
-    while(cadena[i]==' ')
+
+// ----- FUNCION PARA QUITAR ESPACIOS AL INICIO -----
+void quitarEspaciosInicio(char *cadena)
+{
+    int i = 0;
+
+    while(cadena[i] == ' ')
         i++;
 
-    if(i>0){
-        int j=0;
+    if(i > 0){
+        int j = 0;
+
         while(cadena[i] != '\0'){
             cadena[j++] = cadena[i++];
         }
+
         cadena[j] = '\0';
     }
 }
 
-// eliminar vocales
-void quitarVocales(char *cadena){
-    int i=0,j=0;
+
+// ----- FUNCION PARA QUITAR VOCALES -----
+void quitarVocales(char *cadena)
+{
+    int i = 0, j = 0;
     char temp[50];
 
-    while(cadena[i] != '\0'){
+    while(cadena[i] != '\0')
+    {
         char c = tolower(cadena[i]);
 
-        if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u'){
+        if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u')
+        {
             temp[j++] = cadena[i];
         }
+
         i++;
     }
-    temp[j]='\0';
-    strcpy(cadena,temp);
+
+    temp[j] = '\0';
+
+    strcpy(cadena, temp);
 }
 
-// CAPTURAR
-void capturar(PC *pc){
+
+
+// ----- CAPTURAR DATOS -----
+void capturar(PC *pc)
+{
     cin.ignore();
 
     cout<<"Marca: ";
     cin.getline(pc->marca,50);
+
     quitarEspaciosInicio(pc->marca);
     quitarVocales(pc->marca);
 
     cout<<"Modelo: ";
     cin.getline(pc->modelo,50);
+
     quitarEspaciosInicio(pc->modelo);
 
-    cout<<"Memoria RAM (GB): ";
+    cout<<"RAM (GB): ";
     cin>>pc->ram;
 
     cout<<"Velocidad procesador (GHz): ";
@@ -75,6 +96,7 @@ void capturar(PC *pc){
 
     cout<<"Serie tarjeta: ";
     cin.getline(pc->tarjeta.serie,50);
+
     quitarEspaciosInicio(pc->tarjeta.serie);
 
     cout<<"Numero de puertos: ";
@@ -90,15 +112,20 @@ void capturar(PC *pc){
     cin>>pc->tarjeta.largo;
 }
 
-// MOSTRAR
-void mostrar(PC *pc){
+
+
+// ----- MOSTRAR DATOS -----
+void mostrar(PC *pc)
+{
     cout<<"\n--- DATOS PC ---\n";
+
     cout<<"Marca: "<<pc->marca<<endl;
     cout<<"Modelo: "<<pc->modelo<<endl;
     cout<<"RAM: "<<pc->ram<<" GB"<<endl;
     cout<<"Velocidad Procesador: "<<pc->velocidadProcesador<<" GHz"<<endl;
 
     cout<<"\n--- TARJETA ---\n";
+
     cout<<"Serie: "<<pc->tarjeta.serie<<endl;
     cout<<"Numero Puertos: "<<pc->tarjeta.numPuertos<<endl;
     cout<<"Voltaje: "<<pc->tarjeta.voltaje<<endl;
@@ -106,8 +133,11 @@ void mostrar(PC *pc){
     cout<<"Largo: "<<pc->tarjeta.largo<<endl;
 }
 
-// MODIFICAR
-void modificar(PC *pc){
+
+
+// ----- MODIFICAR DATOS -----
+void modificar(PC *pc)
+{
     int op;
 
     cout<<"\n--- MODIFICAR ---\n";
@@ -125,7 +155,8 @@ void modificar(PC *pc){
 
     cin.ignore();
 
-    switch(op){
+    switch(op)
+    {
         case 1:
             cout<<"Nueva marca: ";
             cin.getline(pc->marca,50);
@@ -136,7 +167,6 @@ void modificar(PC *pc){
         case 2:
             cout<<"Nuevo modelo: ";
             cin.getline(pc->modelo,50);
-            quitarEspaciosInicio(pc->modelo);
         break;
 
         case 3:
@@ -152,7 +182,6 @@ void modificar(PC *pc){
         case 5:
             cout<<"Nueva serie: ";
             cin.getline(pc->tarjeta.serie,50);
-            quitarEspaciosInicio(pc->tarjeta.serie);
         break;
 
         case 6:
@@ -180,11 +209,19 @@ void modificar(PC *pc){
     }
 }
 
-int main(){
+
+
+// ----- FUNCION PRINCIPAL -----
+int main()
+{
     PC pc;
+
     int opcion;
 
-    do{
+    bool hayDatos = false; // VARIABLE para saber si ya se capturaron datos
+
+    do
+    {
         cout<<"\n--- MENU ---\n";
         cout<<"1. Capturar\n";
         cout<<"2. Mostrar\n";
@@ -193,13 +230,29 @@ int main(){
         cout<<"Opcion: ";
         cin>>opcion;
 
-        switch(opcion){
-            case 1: capturar(&pc); break;
-            case 2: mostrar(&pc); break;
-            case 3: modificar(&pc); break;
+        switch(opcion)
+        {
+            case 1:
+                capturar(&pc);
+                hayDatos = true; // ahora sabemos que ya hay datos
+            break;
+
+            case 2:
+                if(hayDatos)
+                    mostrar(&pc);
+                else
+                    cout<<"\nNo hay datos para mostrar\n"; // evita números basura
+            break;
+
+            case 3:
+                if(hayDatos)
+                    modificar(&pc);
+                else
+                    cout<<"\nNo hay datos para modificar\n";
+            break;
         }
 
-    }while(opcion != 4);
+    } while(opcion != 4);
 
     return 0;
 }
